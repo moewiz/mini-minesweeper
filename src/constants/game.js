305576fd297c757111bfb77b4550c939b04d3a71
Khoa Nguyen <meo.wizard@gmail.com>
2020-02-15 14:12:15 +0700

    Add constant define game

---
+++ src/constants/game.js
@@ -0,0 +1,16 @@
+export const BEGINNER = {
+  size: 9,
+  mines: 10
+};
+
+export const ADVANTAGE = {
+  size: 16,
+  mines: 40
+};
+
+export const GAME_STATUS = {
+  LOSE: -1,
+  NOT_START: 0,
+  PLAYING: 1,
+  WIN: 2
+};
